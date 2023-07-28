671dc62ac66f8f7bf294c64f1790de0e113bfab6
Dmitry Bubblebass <dmitry.bubblebass@gmail.com>
2023-07-28 18:32:57 +0300

    tests 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,7 +5,7 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 def test_get_error_out_of_range():
     with pytest.raises(IndexError):
@@ -15,3 +15,7 @@ def test_get_error_out_of_range():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+    assert arrs.my_slice([1, 2, 3, 4], 0, -2) == [1, 2]
+    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
